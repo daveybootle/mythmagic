a30895399c76c67afe649f32cdfead5400a8ed94
Kat Crichton-Seager <kat@crichton-seager.com>
2012-04-13 23:29:46 +0100

    Removed unneeded railsy stuff

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Mythmagic::Application.config.session_store :cookie_store, :key => '_mythmagic_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Mythmagic::Application.config.session_store :active_record_store
--- fixchans.rb
+++ fixchans.rb
@@ -1,1 +1,13 @@
-MagicMythMapper.new()
+#!/usr/bin/env ruby
+
+require 'active_record'
+require 'yaml'
+
+dbconfig = YAML::load(File.open('config/database.yml'))
+ActiveRecord::Base.establish_connection(dbconfig['development'])
+
+Dir[File.expand_path('../classes/*.rb', __FILE__)].each{ |file| require file }
+
+mmm = MagicMythMapper.new()
+mmm.report
+
