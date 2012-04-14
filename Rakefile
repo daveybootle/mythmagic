4393c41fb7956bae4bcb88dda719569d8b74d474
Kat Crichton-Seager <kat@crichton-seager.com>
2012-04-14 12:14:01 +0100

    Working Rakefile that runs all the tests

--- Rakefile
+++ Rakefile
@@ -1,3 +1,10 @@
 #!/usr/bin/env rake
+require 'rake/testtask'
 
+task :default => [:test]
 
+Rake::TestTask.new do |t|
+  t.warning = false
+  t.verbose = false
+  t.test_files = FileList['test/*_test.rb']
+end
--- test/magic_myth_mapper_test.rb
+++ test/magic_myth_mapper_test.rb
@@ -3,7 +3,7 @@ require File.expand_path('../test_helper.rb', __FILE__)
 class MagicMythMapperTest < ActiveSupport::TestCase
 
   test "Something" do
-    unit = MagicMythMapper.new()
+#    unit = MagicMythMapper.new()
     assert true
   end
   
